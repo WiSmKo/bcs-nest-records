c131755e85033cc5c287973bf5f579eff3c97d90
WiSmKo <william@thewhisk.dev>
2023-10-14 09:41:03 +0200

    discogs-client service findRecords method

--- src/discogs-client/discogs-client.service.ts
+++ src/discogs-client/discogs-client.service.ts
@@ -11,12 +11,31 @@ export class DiscogsClientService {
     constructor(private readonly httpService: HttpService) {}
 
     async findRecords(title: string, artist: string, year: string, label: string): Promise<DiscogsResponse>{
-        console.log(`Discogs Client Service: Searching for records with queries: title: [${title}], artist: [${artist}], year: [${year}], label: [${label}]`);
-        console.log('/database/search?type=release&format=vinyl&artist='+artist+'&release_title='+title+'&year='+year+'&label='+label);
+
+        let database_search_url: string = '/database/search?';
+
+        const queryParams = {
+            type: "release",
+            format: "vinyl",
+            title: title,
+            artist: artist,
+            year: year,
+            label: label
+        }
+
+
+        const urlSearchParams = new URLSearchParams();
+        for(const [key, value] of Object.entries(queryParams)){
+            if (value !== undefined){
+                urlSearchParams.append(key, value);
+            }
+        }
+
+        console.log(`Discogs Client Service: Searching for records with parameters: ${urlSearchParams.toString()}`);
+
         //DO We WANT THIS TO BE firstValueFrom?
         const { data } = await firstValueFrom(
-            this.httpService.get<DiscogsResponse>(process.env.DISCOGS_BASE_URL+'/database/search?type=release&format=vinyl&artist='+artist
-            +'&release_title='+title+'&year='+year+'&label='+label, 
+            this.httpService.get<DiscogsResponse>(process.env.DISCOGS_BASE_URL+database_search_url.concat(urlSearchParams.toString()),
             {headers: {'Authorization' : 'Discogs token='+process.env.DISCOGS_TOKEN}}).pipe(
                 catchError((error: AxiosError) => {
                     console.log(error.response.data);
@@ -26,5 +45,6 @@ export class DiscogsClientService {
         )
         return data;
     }
+
 }
 
