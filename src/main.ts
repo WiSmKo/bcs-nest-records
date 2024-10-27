04b1c77260ab6ae13bb95fd22319d3ef9b9d57aa
WiSmKo <william@thewhisk.dev>
2024-10-27 10:29:41 +0100

    update port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({origin: process.env.FRONTEND});
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
