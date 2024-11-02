550fcedf916dbea684b9b2fb268a0934da7a7513
WiSmKo <william@thewhisk.dev>
2024-11-02 15:48:17 +0100

    test

--- src/musicbrainz/musicbrainz.service.spec.ts
+++ src/musicbrainz/musicbrainz.service.spec.ts
@@ -43,6 +43,49 @@ describe('MusicbrainzService', () => {
       expect(httpService.get).toHaveBeenCalledTimes(1);
       expect(httpService.get).toHaveBeenCalledWith(url);
     });
+
+    it('should return a ReleaseGroupSearchResponse', async () => {
+      const findRecordsDto = new FindRecordsDto();
+      findRecordsDto.title = 'some title';
+      findRecordsDto.artist = 'some artist';
     
-  });
-});
+      (httpService.get as jest.Mock).mockReturnValueOnce(of({
+        data: {
+          created: '2022-01-01',
+          count: 1,
+          offset: 0,
+          'release-groups': [
+            {
+              id: '123',
+              score: '100',
+              count: 1,
+              title: 'some title',
+              'first-release-date': '2022-01-01',
+              'primary-type': 'Album',
+              'artist-credit': [
+                {
+                  name: 'some artist',
+                },
+              ],
+              releases: [
+                {
+                  id: '123',
+                  title: 'some title',
+                  status: 'Official',
+                },
+              ],
+            },
+          ],
+        },
+      }));
+    
+      const result = await service.findReleaseGroup(findRecordsDto);
+      expect(result).toBeInstanceOf(Object);
+      expect(result).toEqual(expect.objectContaining({
+        created: expect.any(String),
+        count: expect.any(Number),
+        offset: expect.any(Number),
+        "release-groups":expect.any(Array),
+      }));
+  })
+})});
